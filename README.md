54ab5d6cffccedce6bfc528b610985ca19944c62
Jim Verheijde <jim.verheijde@gmail.com>
2018-10-15 23:24:47 +0800

    Partially implemented line wrapping

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 
 This is a useless cli tool of a cat echoing the standard input.
 
+## Operation
+You can either provide your text: 
+
+- Like separate arguments 
+
+`catsay hello there`
+
+- As a single string 
+
+`catsay "hello there"`
+
+- As standard input 
+
+`echo "hello there" | catsay`
+
+For help run either:
+- `catsay -h`  
+- `catsay --help`
+
+Note: when no argument is given (so just `catsay`) the program assumes the input will come from stdin so it will block until it receives an EOF (Ctrl+D)
+
+## Build instructions
+To build this project you need the Rust language installed:
+
+`cargo build`
+
+And to run it:
+
+`cargo run hello there`
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 use std::env;
 use std::io::{self, Read};
 
-const HELP_MESSAGE: &str = "catsay v0.1";
+const HELP_MESSAGE: &str = "catsay v0.1 - This is supposed to help";
+
+const CAT_ASCII_ART: &str = r##"   \                             _
+    \                           | \
+     \                          | |
+      \                         | |
+       \   |\                   | |
+        \ /, ~\                / /
+         X     `-.....-------./ /
+          ~-. ~  ~              |
+             \             /    |
+              \  /_     ___\   /
+              | /\ ~~~~~   \ |
+              | | \        || |
+              | |\ \       || )
+             (_/ (_/      ((_/"##;
 
 fn main() -> Result<(), std::io::Error> {
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
@@ -14,9 +30,33 @@ fn main() -> Result<(), std::io::Error> {
     }
 }
 
+/// Wraps the message into lines of certain width using a greedy algorithm
+fn wrap_lines(message: &str, width: int) -> Vec<&str> {
+    if message.len() <= width {
+        return vec![message];
+    } else {
+        let mut lines: Vec<&str> = vec![];
+        let mut cur_pos: int = width;
+
+        // Search for first whitespace from width to the left
+        while message[cur_pos] != " " {
+            cur_pos -= 1;
+        }
+
+        lines.push(&message[0..cur_pos]);
+
+        let mut rest: Vec<&str> = wrap_lines(&message[cur_pos..message.len()], width);
+
+        // TODO
+        lines.append(&rest);
+
+        return lines;
+    }
+}
+
 /// Print the message with the cat saying it
 fn catsays(message: &str) {
-    println!("Cat says: {}", message);
+    println!("{text}\n{cat}", text=message, cat=CAT_ASCII_ART);
 }
 
 /// Processes the argument input
@@ -35,6 +75,6 @@ fn process_stdin() -> io::Result<()> {
     let mut handle = stdin.lock();
 
     handle.read_to_string(&mut buffer)?;
-    catsays(&buffer);
+    catsays(&buffer.trim());
     Ok(())
 }
